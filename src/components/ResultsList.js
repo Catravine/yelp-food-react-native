6490d541caab6c7f047929406aa48c72c02c7cc7
Catravine <caroline@carolinecourtney.com>
2023-06-11 14:56:11 -0400

    decided to filter within search screen, pass to results list children

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const ResultsList = ({ title }) => {
+const ResultsList = ({ title, results }) => {
 
   return (
     <View>
       <Text style={styles.title}>{title}</Text>
+      <Text>Results: {results.length}</Text>
     </View>
   )
 }

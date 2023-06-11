cf24c58d58d009366a6af40bf80908547a3c1b2b
Catravine <caroline@carolinecourtney.com>
2023-06-11 14:33:58 -0400

    useEffect to call default search on start, only once!

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -6,26 +6,36 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        term, // key has same name as variable for it
-        location: 'san jose',
-        limit: 50
-      }
-    });
-    setResults(response.data.businesses);
-  }
+  const searchApi = async (searchTerm) => {
+    setErrorMessage('')
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          term: searchTerm, 
+          location: 'san jose',
+          limit: 50
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
+  };
+
+  useEffect(() => {
+    searchApi('pasta')
+  }, []);
 
   return (
     <View>
       <SearchBar 
         term={term} 
         onTermChange={setTerm} 
-        onTermSubmit={searchApi}
+        onTermSubmit={() => searchApi(term)}
       />
-      <Text>SearchScreen: Term = {term}</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results.</Text>
     </View>
   )

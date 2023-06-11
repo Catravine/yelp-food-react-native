fe273ff3016cf4bb17b19eadd4fc230aaf0d7c30
Catravine <caroline@carolinecourtney.com>
2023-06-11 14:41:59 -0400

    extracted api call to a custom hook

---
+++ src/hooks/useResults.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import yelp from '../api/yelp';
+
+export default () => {
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async (searchTerm) => {
+    setErrorMessage('')
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          term: searchTerm, 
+          location: 'san jose',
+          limit: 50
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
+  };
+
+  useEffect(() => {
+    searchApi('pasta')
+  }, []);
+
+  return [searchApi, results, errorMessage]
+}
